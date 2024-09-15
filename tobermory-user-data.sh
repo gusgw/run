e521942400991df1a6a9070df87816b0f4690aac
Angus Gray-Weale <gusgw@gusgw.net>
2024-09-15 22:32:08 +1000

    Set output files correctly

--- tobermory-user-data.sh
+++ tobermory-user-data.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-apt -y update 1> ~/apt.out 2> ~/apt.update.err
+apt -y update 1> ~/apt.update.out 2> ~/apt.update.err
 apt -y upgrade 1> ~/apt.upgrade.out 2> ~/apt.upgrade.err
 
 for pkg in git bc gnupg2 rclone parallel; do
-    apt -y install "${pkg}" 1> "apt.${pkg}.out" 2> "apt.${pkg}.err"
+    apt -y install "${pkg}" 1> "~/apt.${pkg}.out" 2> "~/apt.${pkg}.err"
 done
