3405fa0f214ec83eefadc2d6523731cbc8de4ca2
Angus Gray-Weale <gusgw@gusgw.net>
2024-09-20 21:07:15 +1000

    Configure script code moved to user data

--- worker/configure.sh
+++ worker/configure.sh
@@ -11,52 +11,52 @@ SSHCMD="ssh -i ~/.ssh/${key}"
 SCPCMD="scp -i ~/.ssh/${key}"
 WORKER="admin@${ip}"
 
-while ! ${SSHCMD} ${WORKER} which git; do
-    sleep 5
-done
+# while ! ${SSHCMD} ${WORKER} which git; do
+#     sleep 5
+# done
 
-${SSHCMD} ${WORKER} << ENDSSH
-rm -rf ~/.ssh/${deploy}
-rm -rf ~/.ssh/config
-rm -rf ~/.gnupg
-ENDSSH
+# ${SSHCMD} ${WORKER} << ENDSSH
+# rm -rf ~/.ssh/${deploy}
+# rm -rf ~/.ssh/config
+# rm -rf ~/.gnupg
+# ENDSSH
 
-${SCPCMD} ~/.ssh/${deploy} "${WORKER}:~/.ssh/"
-${SCPCMD} ${asc}/*.asc "${WORKER}:~/"
-${SCPCMD} ${rclone} "${WORKER}:~/"
+# ${SCPCMD} ~/.ssh/${deploy} "${WORKER}:~/.ssh/"
+#${SCPCMD} ${asc}/*.asc "${WORKER}:~/"
+# ${SCPCMD} ${rclone} "${WORKER}:~/"
 
 ${SSHCMD} "${WORKER}" << ENDSSH
-chmod og-rw ~/.ssh/${deploy}
-cat > ~/.ssh/config << EOF
-Host github
-    HostName        github.com
-    User            git
-    Port            22
-    IdentityFile    ~/.ssh/id_ed25519-tobermory-github_deploy
-EOF
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+# chmod og-rw ~/.ssh/${deploy}
+# cat > ~/.ssh/config << EOF
+# Host github
+#     HostName        github.com
+#     User            git
+#     Port            22
+#     IdentityFile    ~/.ssh/${deploy}
+# EOF
+# ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
-mkdir -p ~/src/
-cd ~/src/
-rm -rf run
-git clone -b ${branch} github:gusgw/run.git 1> ~/git.clone.out 2> ~/git.clone.err
-cd run
-git submodule update --init --recursive 1> ~/git.sub.out 2> ~/git.sub.err
+# mkdir -p ~/src/
+# cd ~/src/
+# rm -rf run
+# git clone -b ${branch} github:gusgw/run.git 1> ~/git.clone.out 2> ~/git.clone.err
+# cd run
+# git submodule update --init --recursive 1> ~/git.sub.out 2> ~/git.sub.err
 
-rm rclone.conf
-ln -s ~/${rclone##*/} rclone.conf
+# rm rclone.conf
+# ln -s ~/${rclone##*/} rclone.conf
 
-cd
-gpg --import *.asc
+# cd
+# gpg --import *.asc
 
-sudo mkfs.ext4 /dev/nvme1n1
-sudo mkdir -p /mnt/data
-sudo mount /dev/nvme1n1 /mnt/data
-sudo chown admin /mnt/data
+# sudo mkfs.ext4 /dev/nvme1n1
+# sudo mkdir -p /mnt/data
+# sudo mount /dev/nvme1n1 /mnt/data
+# sudo chown admin /mnt/data
 
-mkdir bin
-cd bin
-wget  http://s3.amazonaws.com/ec2metadata/ec2-metadata
-chmod u+x ./ec2-metadata
-ln -s ../src/run/run.sh run
+# mkdir bin
+# cd bin
+# wget  http://s3.amazonaws.com/ec2metadata/ec2-metadata
+# chmod u+x ./ec2-metadata
+# ln -s ../src/run/run.sh run
 ENDSSH
